3b993c6d3315523b95a0caa6d17740610f007cf7
Tim Park <timfpark@gmail.com>
2022-06-11 13:52:03 -0700

    Add core model reconciling processor for deployment create/update/delete

--- src/service/processor/core.rs
+++ src/service/processor/core.rs
@@ -60,8 +60,15 @@ impl Processor for CoreProcessor {
 }
 
 impl CoreProcessor {
-    /*
-    async fn reconcile_deployment(&self, deployment: &Deployment) -> anyhow::Result<()> {
+    async fn process_deployment_event(&self, event: &Event) -> anyhow::Result<()> {
+        let deployment: Deployment = DeploymentMessage::decode(&*event.serialized_model)?.into();
+
+        let desired_hosts: usize = if event.event_type == EventType::Deleted as i32 {
+            0
+        } else {
+            deployment.hosts as usize
+        };
+
         let target = self.target_service.get_by_id(&deployment.target_id).await?;
         let target = match target {
             Some(target) => target,
@@ -72,16 +79,19 @@ impl CoreProcessor {
                 ))
             }
         };
-        let mut matching_hosts = self.host_service.get_matching_target(&target).await?;
+
+        let mut available_hosts = self.host_service.get_matching_target(&target).await?;
+
         let mut current_assignments = self
             .assignment_service
             .get_by_deployment_id(&deployment.id)
             .await?;
 
+        let mut assignments_to_create = Vec::new();
         let mut assignments_to_delete = Vec::new();
 
         for assignment in current_assignments.iter() {
-            if matching_hosts
+            if available_hosts
                 .iter()
                 .filter(|host| host.id == assignment.host_id)
                 .count()
@@ -90,71 +100,42 @@ impl CoreProcessor {
                 assignments_to_delete.push(assignment.id.clone());
             } else {
                 // host still matches target, so remove it from matching_hosts so we don't double assign it.
-                matching_hosts.retain(|host| host.id == assignment.host_id);
+                available_hosts.retain(|host| host.id == assignment.host_id);
             }
         }
 
-        current_assignments.retain(|assignment| !assignments_to_delete.contains(&assignment.id));
+        if current_assignments.len() > desired_hosts {
+            let delete_count = current_assignments.len() - desired_hosts;
 
-        Ok(())
-    }
-    */
+            let delete_assignment_ids: Vec<String> = current_assignments
+                .drain(0..delete_count)
+                .map(|assignment| assignment.id)
+                .collect();
 
-    async fn process_deployment_event(&self, event: &Event) -> anyhow::Result<()> {
-        let event_type = event.event_type;
-        match event_type {
-            event_type if event_type == EventType::Created as i32 => {
-                let deployment: Deployment =
-                    DeploymentMessage::decode(&*event.serialized_model)?.into();
+            assignments_to_delete.extend(delete_assignment_ids);
+        } else {
+            let create_count = desired_hosts - current_assignments.len();
 
-                let target = self.target_service.get_by_id(&deployment.target_id).await?;
-                let target = match target {
-                    Some(target) => target,
-                    None => {
-                        return Err(anyhow::anyhow!(
-                            "couldn't find target with id {}",
-                            deployment.target_id
-                        ))
-                    }
-                };
+            assignments_to_create = available_hosts
+                .drain(0..create_count)
+                .map(|host| Assignment {
+                    id: Assignment::make_id(&deployment.id, &host.id),
+                    deployment_id: deployment.id.clone(),
+                    host_id: host.id.clone(),
+                })
+                .collect();
+        }
 
-                let mut matching_hosts = self.host_service.get_matching_target(&target).await?;
-                let hosts_usize = deployment.hosts as usize;
+        for assignment in assignments_to_create {
+            self.assignment_service
+                .create(&assignment, &event.operation_id)
+                .await?;
+        }
 
-                let deployment_hosts = match deployment.hosts {
-                    0 => matching_hosts,
-                    _ => matching_hosts.drain(0..hosts_usize).collect(),
-                };
-
-                let new_assignments: Vec<Assignment> = deployment_hosts
-                    .iter()
-                    .map(|host| Assignment {
-                        id: Assignment::make_id(&deployment.id, &host.id),
-                        deployment_id: deployment.id.clone(),
-                        host_id: host.id.clone(),
-                    })
-                    .collect();
-
-                for new_assignment in new_assignments.iter() {
-                    self.assignment_service
-                        .create(new_assignment, &event.operation_id)
-                        .await?;
-                }
-
-                tracing::info!("deployment created (NOP): {:?}", event);
-            }
-            event_type if event_type == EventType::Updated as i32 => {
-                // Render deployment and commit in GitOps folder
-                tracing::info!("deployment updated (NOP): {:?}", event);
-            }
-            event_type if event_type == EventType::Deleted as i32 => {
-                // Delete assignments for deployment (Assignment Deleted event will handle unlinking host from deployment)
-                // Remove deployment from GitOps folder
-                tracing::info!("deployment deleted (NOP): {:?}", event);
-            }
-            _ => {
-                panic!("unsupported event type: {:?}", event);
-            }
+        for assignment in assignments_to_delete {
+            self.assignment_service
+                .delete(&assignment, &event.operation_id)
+                .await?;
         }
 
         Ok(())
