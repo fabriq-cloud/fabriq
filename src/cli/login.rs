f831348cbbef2950a4a7d4ae55a5d75666b04f82
Tim Park <timfpark@gmail.com>
2022-11-10 16:54:46 -0600

    Don't fetch user metadata at login (doesn't work with Github Action PAT)

--- src/cli/login.rs
+++ src/cli/login.rs
@@ -14,15 +14,16 @@ pub async fn handlers(model_match: &clap::ArgMatches, _context: &Context) -> any
         .expect("GitHub Personal Access Token expected")
         .to_string();
 
-    let octocrab = octocrab::OctocrabBuilder::new()
-        .personal_token(pat.clone())
-        .build()?;
+    /*
+        let octocrab = octocrab::OctocrabBuilder::new()
+            .personal_token(pat.clone())
+            .build()?;
 
-    let github_user = octocrab.current().user().await?;
-
+        let github_user = octocrab.current().user().await?;
+    */
     let profile = Profile {
         pat: Some(pat),
-        login: Some(github_user.login),
+        login: None, // Some(github_user.login),
     };
 
     profile.save()?;
