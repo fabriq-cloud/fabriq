bb2cde00f675987402fce076d899317c45067a1c
Tim Park <timfpark@gmail.com>
2022-11-15 16:49:13 -0800

    Add health endpoint test (#35)

--- src/service/http/health.rs
+++ src/service/http/health.rs
@@ -4,3 +4,35 @@ use axum::response::Html;
 pub async fn health() -> Html<&'static str> {
     Html("ok")
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{self, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    use crate::http::http_router;
+
+    #[tokio::test]
+    async fn test_health() {
+        let app = http_router();
+
+        let response = app
+            .oneshot(
+                http::Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/health")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"ok");
+    }
+}
--- src/service/http/mod.rs
+++ src/service/http/mod.rs
@@ -1,2 +1,10 @@
+use axum::{routing::get, Router};
+
 pub mod health;
 pub mod webhook;
+
+pub fn http_router() -> Router {
+    Router::new()
+        .route("/health", get(health::health))
+        .route("/event_handler", get(webhook::event_handler))
+}
